21737fdf22aaf3c0a8813076a8d765a24fea4c89
meenu618 <menuka.jain@gmail.com>
2024-09-16 15:07:17 +0100

    Update hello.py
    Added CSV file from internet on some GP. You can change the csv file, this was done to test if it works

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
 print("hello")
 import panda as pd
-## df = pd.read_csv()
-## df.head()
+df = pd.read_csv("https://digital.nhs.uk/services/organisation-data-service/export-data-files/csv-downloads/gp-and-gp-practice-related-data.csv")
+df.head()
